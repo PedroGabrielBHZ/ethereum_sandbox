d7bc8d029fe1c6a1f5323f3afbb4a60a33f6665b
Pedro Gabriel Amorim Soares <pedrogabrielbhz@gmail.com>
2021-07-18 17:32:18 -0300

    deploy and test lottery contract

---
+++ lottery/deploy.js
@@ -0,0 +1,24 @@
+const HDWalletProvider = require("truffle-hdwallet-provider");
+const Web3 = require("web3");
+const { interface, bytecode } = require("./compile");
+
+const provider = new HDWalletProvider(
+  "believe rural rent head say axis nation exhibit peanut author purse gallery",
+  // remember to change this to your own phrase!
+  "https://rinkeby.infura.io/v3/eb4f31bb234f41b396cd0c190aa4b1b0"
+  // remember to change this to your own endpoint!
+);
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+
+  console.log("Attempting to deploy from account", accounts[0]);
+
+  const result = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode })
+    .send({ gas: "1000000", from: accounts[0] });
+
+  console.log("Contract deployed to", result.options.address);
+};
+deploy();
